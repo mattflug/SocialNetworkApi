c57b34ca8cded9eabbd0fc782a36859744a5a6e1
Ghost <mbflug@gmail.com>
2022-10-08 12:37:20 -0600

    pushing timestamp

--- models/Reactions.js
+++ models/Reactions.js
@@ -1,26 +1,27 @@
-const { Schema, Types } = require('mongoose');
+var moment = require("moment"); // require
+moment().format();
+const { Schema, Types } = require("mongoose");
 
-const assignmentSchema = new Schema(
+const reactionSchema = new Schema(
   {
-    assignmentId: {
+    reactionId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId(),
     },
-    assignmentName: {
+    reactionBody: {
       type: String,
       required: true,
-      maxlength: 50,
-      minlength: 4,
-      default: 'Unnamed assignment',
+      maxlength: 280,
     },
-    score: {
-      type: Number,
+    username: {
+      type: String,
       required: true,
-      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
     },
     createdAt: {
       type: Date,
       default: Date.now,
+      get: (timestamp) =>
+        moment(timestamp).format("dddd, MMMM Do YYYY, h:mm:ss a"),
     },
   },
   {
