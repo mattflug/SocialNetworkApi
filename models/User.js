a0b609cd32c6a47d0678b924ffcb72f3fc560e8b
Ghost <mbflug@gmail.com>
2022-10-08 12:27:18 -0600

    first push

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+const assignmentSchema = require("./Assignment");
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\.com/, "Please type valid email"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length
+})
+
+const User = model("student", userSchema);
+
+module.exports = User;
